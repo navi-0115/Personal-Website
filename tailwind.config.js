4d89818d23175489e520be83837c2cca511970c3
Raden-nvii <navinugraha15@gmail.com>
2024-06-22 16:38:17 +0700

    chore: update images in the asset folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'main': "url('assets/images/main-bg.jpg')",
+      },
+    },
   },
   plugins: [],
 }
